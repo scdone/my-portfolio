3ed362383661647874c5927d2e12efdababbace3
Stetson Done <scdone@gmail.com>
2022-01-25 22:08:17 -0700

    framework html and css completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='main'>
+      <section id='img-div'>
+      </section>
+
+
+      <section>
+        <div className='box middle green'>
+          <h1>Stetson Done</h1>
+        </div>
+        <div className='box middle dark-blue'>
+          <h2>projects</h2>
+        </div>
+      </section>
+
+
+      <section>
+        <div className='box right dark-blue'>
+          <h2>contact</h2>
+        </div>
+        <div className='box right middle-blue'>
+          <h2>experience</h2>
+        </div>
+        <div className='box right green'>
+          <h2>skills</h2>
+          <div className='skill-icon-container'><img className='skill-icons'src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-plain-wordmark.svg" />
+          <img className='skill-icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
+          <img className='skill-icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
+          <img className='skill-icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg" />
+          <img className='skill-icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg" />
+          <img className='skill-icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg" /></div>
+        </div>
+      </section>
+
+    </main>
   );
 }
 
