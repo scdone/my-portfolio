b9ac6067a781a86ad5408588a28cc47baeeb198b
Stetson Done <scdone@gmail.com>
2022-01-26 16:13:51 -0700

    added img alt txt

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.scss';
-import { Link } from 'react-router-dom';
 import linkedInIcon from './githublogo.png';
 
 function App() {
@@ -24,8 +23,8 @@ function App() {
               <br/>
               <li>Full-Stack text-based adventure game</li>
               <div className='project-buttons-noble-knight'>
-              <a target='_blank' className='project-links' href="https://playnobleknight.com">see live</a>
-              <a target='_blank' className='project-links' href="https://github.com/scdone/nobleKnight">source code</a>
+              <a target='_blank' rel="noreferrer" className='project-links' href="https://playnobleknight.com">see live</a>
+              <a target='_blank' rel="noreferrer" className='project-links' href="https://github.com/scdone/nobleKnight">source code</a>
               </div>
               <p>Created with React, Node.js, JavaScript, HTML5, CSS, PostgreSQL database, and Sequelize</p>
             </div>
@@ -37,10 +36,10 @@ function App() {
         <div className='box right dark-blue'>
           <h2>contact</h2>
           <div className='contact-box'>
-            <img className='contact-img' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" />
-            <img className='contact-img' src={linkedInIcon}></img>
-              <a target="_blank" className='contact-links' href="https://www.linkedin.com/in/stetson-done/">LinkedIn</a>
-              <a target="_blank" className='contact-links' href="https://github.com/scdone">GitHub</a>
+            <img className='contact-img' alt="linkedin icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" />
+            <img className='contact-img' src={linkedInIcon} alt="github icon"></img>
+              <a target="_blank" rel="noreferrer" className='contact-links' href="https://www.linkedin.com/in/stetson-done/">LinkedIn</a>
+              <a target="_blank" rel="noreferrer" className='contact-links' href="https://github.com/scdone">GitHub</a>
           </div>
         </div>
         <div className='box right middle-blue'>
@@ -51,12 +50,12 @@ function App() {
         </div>
         <div className='box right green'>
           <h2>skills</h2>
-          <div className='skill-icon-container'><img className='skill-icons'src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-plain-wordmark.svg" />
-          <img className='skill-icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
-          <img className='skill-icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
-          <img className='skill-icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg" />
-          <img className='skill-icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg" />
-          <img className='skill-icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg" /></div>
+          <div className='skill-icon-container'><img className='skill-icons'src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-plain-wordmark.svg" alt="postgresQL icon" />
+          <img className='skill-icons' alt="react icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
+          <img className='skill-icons' alt="javascript icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
+          <img className='skill-icons' alt="html5 icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg" />
+          <img className='skill-icons' alt="nodeJS icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg" />
+          <img className='skill-icons' alt="css3 icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg" /></div>
         </div>
       </section>
 
