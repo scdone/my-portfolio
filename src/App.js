f6b6dfb9311e2b1b7d32f92c3e66a7d5e0a36791
Stetson Done <scdone@gmail.com>
2022-02-17 16:39:57 -0700

    edited paragraph and image spacing

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
         </div>
         <div className='box right middle-blue'>
           <h2>experience</h2>
-          <p className='bio-paragraph'>My passion lies in making data make sense. I have experience as a data analyst using Python, Pandas, Matplotlib, SQL, plus many other packages in the Anaconda distribution and beyond. In addition, I have experience as a fullstack developer using React, Javascript, HTML, CSS, Node.js, and SQL. Previous to software development, I worked as a Deputy Sheriff in Phoenix, Arizona.
+          <p className='bio-paragraph'>My passion lies in making data make sense. I have experience as a data analyst using Python, Pandas, Matplotlib, other packages in the Anaconda distribution, and beyond. In addition, I have experience as a fullstack developer using React, Javascript, HTML and CSS. Previous to software development, I worked as a Deputy Sheriff in Phoenix, Arizona.
           </p>
           <a target="_blank" rel="noreferrer" className='contact-links' href="https://docs.google.com/document/d/e/2PACX-1vRPzged0CBqgGsXo-shYjmcGYl9xlnJdqbWvbWoV3xR6Ba8Ltczrhw-muzqcI9UNev2ASqpFGanSKO9/pub">see resume</a>
         </div>
