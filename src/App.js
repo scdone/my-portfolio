aad5a2e47d674976fdb473c28c2a9abaaab4e4ac
Stetson Done <scdone@gmail.com>
2022-02-17 22:07:01 -0700

    updated github link for data project

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
               <br/>
               <li>Analysis of FBI gun data and U.S. Census data</li>
               <div className='project-buttons-noble-knight'>
-              <a target='_blank' rel="noreferrer" className='project-links' href="https://github.com/scdone/US-Census-and-Gun-Data-Analysis/blob/main/GunData_Notebook.ipynb">source code</a>
+              <a target='_blank' rel="noreferrer" className='project-links' href="https://github.com/scdone/US-Census-and-Gun-Data-Analysis/blob/main/US_census_gun_data_analysis_notebook.ipynb">source code</a>
               <p>Gathered, cleaned, and analyzed date from the FBI and U.S. Census to identify and visualize trends in gun permits per capita across the U.S.</p>
               </div>
               <br/>
